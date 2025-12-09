49222377ede1f8ad34dc07b7c3eb1854e6356a1d
nicosdiogo <nicosdiogojoao@gmail.com>
2025-12-09 17:50:24 +0000

    segundo commit

--- src/app/Components/LanguageSelect.tsx
+++ src/app/Components/LanguageSelect.tsx
@@ -2,14 +2,11 @@
 
 import { useState } from "react";
 import { useRouter, usePathname } from 'next/navigation';
-
-
-
+import Image from "next/image";
 
 export default function LanguageSelect() {
-
-  const router=useRouter();
-  const pathname=usePathname()
+  const router = useRouter();
+  const pathname = usePathname();
   const [lang, setLang] = useState("pt");
   const [isOpen, setIsOpen] = useState(false);
 
@@ -17,29 +14,25 @@ export default function LanguageSelect() {
     { code: "pt", label: "Português", flag: "https://flagcdn.com/w40/pt.png" },
     { code: "en", label: "English", flag: "https://flagcdn.com/w40/gb.png" },
     { code: "fr", label: "Français", flag: "https://flagcdn.com/w40/fr.png" },
-
   ];
 
   const selected = languages.find((l) => l.code === lang);
 
-  const currentlocale=pathname.split("/")[1]
-
-
   return (
     <div className="relative inline-block text-left">
-     
       <button
         onClick={() => setIsOpen(!isOpen)}
         className="flex items-center gap-2 bg-gray-800 text-white px-3 py-2 rounded-md hover:bg-gray-700 transition-colors"
       >
-        <img 
-          src={selected?.flag} 
-          alt={selected?.label}
-          className="w-8 h-6 object-cover rounded"
+        <Image 
+          src={selected?.flag || ""} 
+          alt={selected?.label || ""} 
+          width={32} 
+          height={24} 
+          className="rounded"
         />
       </button>
 
-     
       {isOpen && (
         <div className="absolute mt-2 w-16 bg-gray-500 rounded-md shadow-lg z-10">
           {languages.map((l) => (
@@ -48,20 +41,22 @@ export default function LanguageSelect() {
               onClick={() => {
                 setLang(l.code);
                 setIsOpen(false); 
-                const parts=pathname.split("/")
-                parts[1]=l.code
-                const newPath =parts.join("/")
-                router.replace(newPath)
+                const parts = pathname.split("/");
+                parts[1] = l.code;
+                const newPath = parts.join("/");
+                router.replace(newPath);
               }}
               className={`flex items-center justify-center w-full px-3 py-2 hover:bg-gray-700 rounded-md transition-colors ${
                 l.code === lang ? 'bg-gray-700' : ''
               }`}
-              title={l.label} // Tooltip mostra o nome ao passar o mouse
+              title={l.label}
             >
-              <img 
+              <Image 
                 src={l.flag} 
-                alt={l.label}
-                className="w-8 h-6 object-cover rounded"
+                alt={l.label} 
+                width={32} 
+                height={24} 
+                className="rounded"
               />
             </button>
           ))}
